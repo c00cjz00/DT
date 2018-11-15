77c224401ae53df05ba06340da34034d3414f685
c00cjz00 <summerhill001@gmail.com>
2018-11-16 02:00:19 +0800

    init

--- init.sh
+++ init.sh
@@ -11,9 +11,10 @@ git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
 #git rm crontab-crontab.php crontab-sftp.php
-git rm --cached myTB_config.php
+#git rm --cached myTB_config.php
+java -jar bfg-1.12.16.jar --delete-files myTB_config.php
 git add *
 git commit -m "init"
 # 上傳至遠端
-git push origin master
-
+#git push origin master
+git push origin --force --all 
