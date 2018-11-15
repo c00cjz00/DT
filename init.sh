44058c838279ceb8f532e4dfd37e10ee5a96a675
c00cjz00 <summerhill001@gmail.com>
2018-11-16 01:50:09 +0800

    init

--- init.sh
+++ init.sh
@@ -10,7 +10,8 @@ git config --global user.name "c00cjz00"
 git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
-git rm crontab-crontab.php crontab-sftp.php
+#git rm crontab-crontab.php crontab-sftp.php
+git rm --cached myTB_config.php
 git add *
 git commit -m "init"
 # 上傳至遠端
